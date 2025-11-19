7ec54c2bcc420038738aa390cdedc8abcc7a1ff6
Maria Eduarda Marques <168608703+dudamarqs@users.noreply.github.com>
2025-11-18 21:14:27 -0300

    Delete src directory

--- src/components/ScriptsAdminPage/Administradores.jsx
+++
@@ -1,3 +0,0 @@
-import React, { useState, useMemo } from "react";
-import "../../styles/StylesAdminPage/adminstradores.css"
-import { FiEdit, FiTrash2, FiMoon, FiPlus, FiSearch } from "react-icons/fi";
